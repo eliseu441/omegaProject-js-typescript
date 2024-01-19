27cf41da40c75d34e76a72c500980059ef66bf3f
eliseu <eliseu441@gmail.com>
2024-01-19 09:59:24 -0300

    backup 0

---
+++ src/components/Pages/Home.jsx
@@ -0,0 +1,257 @@
+import React, { useEffect } from 'react';
+import Card from '../Card';
+import FunFact from '../FunFact';
+import Hero from '../Hero';
+import Div from '../Div';
+import SectionHeading from '../SectionHeading';
+import Spacing from '../Spacing';
+import Cta from '../Cta';
+import LogoList from '../LogoList';
+import MovingText from '../MovingText';
+import PortfolioSlider from '../Slider/PortfolioSlider';
+import PostSlider from '../Slider/PostSlider';
+import TestimonialSlider from '../Slider/TestimonialSlider';
+import TeamSlider from '../Slider/TeamSlider';
+import VideoModal from '../VideoModal';
+import TimelineSlider from '../Slider/TimelineSlider';
+import { pageTitle } from '../../helper';
+
+export default function Home() {
+  pageTitle('Home');
+
+  // Hero Social Links
+  const heroSocialLinks = [
+    {
+      name: 'Instagram',
+      links: '/',
+    },
+    {
+      name: 'Twitter',
+      links: '/',
+    },
+  ];
+
+  // FunFact Data
+  const funfaceData = [
+    {
+      title: 'Com apenas isso voce já pode contratar nossos serviços',
+      factNumber: 'R$399,00',
+    },
+  ];
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  return (
+    <>
+      {/* Start Hero Section */}
+      <Hero
+        title="Tranforme ideias <br/> em sites de impacto."
+        subtitle="Chega de sites pouco detalhados e programação low code pro seu negocio, nosso time cria um site profissional do 0 pra você."
+        btnText="Get a Quote"
+        btnLink="/contact"
+        scrollDownId="#service"
+        socialLinksHeading="Follow Us"
+        heroSocialLinks={heroSocialLinks}
+        bgImageUrl="/images/bg_home.jpg"
+      />
+      {/* End Hero Section */}
+
+      {/* Start FunFact Section */}
+      <div className="container">
+        <FunFact
+          variant="cs-type1"
+          title="Acessivel pra todo tipo de nogócio"
+          data={funfaceData}
+        />
+      </div>
+      {/* End FunFact Section */}
+      {/* Start Portfolio Section */}
+      <Spacing lg="150" md="50" />
+      <Div>
+        <Div className="container">
+          <SectionHeading
+            title="Qualidade e responsividade"
+            subtitle="Ultimos projetos"
+            variant="cs-style1 text-center"
+          />
+          <Spacing lg="90" md="45" />
+        </Div>
+        <PortfolioSlider />
+      </Div>
+      {/* End Portfolio Section */}
+      {/* Start Service Section */}
+      <Spacing lg="150" md="80" />
+      <Div id="service">
+        <Div className="container">
+          <Div className="row">
+            <Div className="col-xl-4">
+              <SectionHeading
+                title="Services we can help you with"
+                subtitle="What Can We Do"
+                btnText="See All Services"
+                btnLink="/service"
+              />
+              <Spacing lg="90" md="45" />
+            </Div>
+            <Div className="col-xl-8">
+              <Div className="row">
+                <Div className="col-lg-3 col-sm-6 cs-hidden_mobile"></Div>
+                <Div className="col-lg-3 col-sm-6">
+                  <Card
+                    title="UI/UX design"
+                    link="/service/service-details"
+                    src="/images/service_1.jpeg"
+                    alt="Service"
+                  />
+                  <Spacing lg="0" md="30" />
+                </Div>
+                <Div className="col-lg-3 col-sm-6 cs-hidden_mobile"></Div>
+                <Div className="col-lg-3 col-sm-6">
+                  <Card
+                    title="React.js Development"
+                    link="/service/service-details"
+                    src="/images/service_2.jpeg"
+                    alt="Service"
+                  />
+                  <Spacing lg="0" md="30" />
+                </Div>
+                <Div className="col-lg-3 col-sm-6">
+                  <Card
+                    title="Digital Marketing"
+                    link="/service/service-details"
+                    src="/images/service_3.jpeg"
+                    alt="Service"
+                  />
+                  <Spacing lg="0" md="30" />
+                </Div>
+                <Div className="col-lg-3 col-sm-6 cs-hidden_mobile"></Div>
+                <Div className="col-lg-3 col-sm-6">
+                  <Card
+                    title="Technology"
+                    link="/service/service-details"
+                    src="/images/service_4.jpeg"
+                    alt="Service"
+                  />
+                  <Spacing lg="0" md="30" />
+                </Div>
+                <Div className="col-lg-3 col-sm-6 cs-hidden_mobile"></Div>
+              </Div>
+            </Div>
+          </Div>
+        </Div>
+      </Div>
+      {/* End Service Section */}
+
+
+
+      {/* Start Awards Section */}
+      <Spacing lg="150" md="80" />
+      <Div className="cs-shape_wrap_2">
+        <Div className="cs-shape_2">
+          <Div />
+        </Div>
+        <Div className="container">
+          <Div className="row">
+            <Div className="col-xl-4">
+              <SectionHeading
+                title="We get multiple awards"
+                subtitle="Our Awards"
+                variant="cs-style1"
+              />
+              <Spacing lg="90" md="45" />
+            </Div>
+            <Div className="col-xl-7 offset-xl-1">
+              <TimelineSlider />
+            </Div>
+          </Div>
+        </Div>
+      </Div>
+      {/* End Awards Section */}
+
+      {/* Start Video Block Section */}
+      <Spacing lg="130" md="70" />
+      <Div className="container">
+        <h2 className="cs-font_50 cs-m0 text-center cs-line_height_4">
+          Our agile process is ability to adapt and respond to change. Agile
+          organizations view change as an opportunity, not a threat.
+        </h2>
+        <Spacing lg="70" md="70" />
+        <VideoModal
+          videoSrc="https://www.youtube.com/watch?v=VcaAVWtP48A"
+          bgUrl="/images/video_bg.jpeg"
+        />
+      </Div>
+      {/* End Video Block Section */}
+
+      {/* Start Team Section */}
+      <Spacing lg="145" md="80" />
+      <Div className="container">
+        <SectionHeading
+          title="Awesome team <br/>members"
+          subtitle="Our Team"
+          variant="cs-style1"
+        />
+        <Spacing lg="85" md="45" />
+        <TeamSlider />
+      </Div>
+      <Spacing lg="150" md="80" />
+      {/* End Team Section */}
+
+      {/* Start Testimonial Section */}
+      <TestimonialSlider />
+      {/* End Testimonial Section */}
+
+      {/* Start Blog Section */}
+      <Spacing lg="150" md="80" />
+      <Div className="cs-shape_wrap_4">
+        <Div className="cs-shape_4"></Div>
+        <Div className="cs-shape_4"></Div>
+        <Div className="container">
+          <Div className="row">
+            <Div className="col-xl-4">
+              <SectionHeading
+                title="Explore recent publication"
+                subtitle="Our Blog"
+                btnText="View More Blog"
+                btnLink="/blog"
+              />
+              <Spacing lg="90" md="45" />
+            </Div>
+            <Div className="col-xl-7 offset-xl-1">
+              <Div className="cs-half_of_full_width">
+                <PostSlider />
+              </Div>
+            </Div>
+          </Div>
+        </Div>
+      </Div>
+      {/* End Blog Section */}
+
+      {/* Start MovingText Section */}
+      <Spacing lg="125" md="70" />
+      <MovingText text="Our reputed world wide partners" />
+      <Spacing lg="105" md="70" />
+      {/* End MovingText Section */}
+
+      {/* Start LogoList Section */}
+      <Div className="container">
+        <LogoList />
+      </Div>
+      <Spacing lg="150" md="80" />
+      {/* End LogoList Section */}
+
+      {/* Start CTA Section */}
+      <Div className="container">
+        <Cta
+          title="Let’s disscuse make <br />something <i>cool</i> together"
+          btnText="Apply For Meeting"
+          btnLink="/contact"
+          bgSrc="/images/cta_bg.jpeg"
+        />
+      </Div>
+      {/* End CTA Section */}
+    </>
+  );
+}
